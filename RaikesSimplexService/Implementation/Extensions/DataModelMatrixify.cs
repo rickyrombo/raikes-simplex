5feabee8248811f7f16f1cb56685f768c47d6b14
Marcus Pasell <marcus.pasell@huskers.unl.edu>
2014-11-15 16:21:51 -0600

    Used matrixify

--- RaikesSimplexService/Implementation/Extensions/DataModelMatrixify.cs
+++ RaikesSimplexService/Implementation/Extensions/DataModelMatrixify.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace RaikesSimplexService.Implementation.Extensions
 {
@@ -10,10 +11,13 @@ namespace RaikesSimplexService.Implementation.Extensions
     {
         public static string Matrixify(this Model model)
         {
-            var rhs = string.Join("\n", model.Constraints.Select(s => s.Value));
-            var lhs = string.Join("\n", model.Constraints.Select(s => string.Join("\t", s.Coefficients)));
-            var objectiveRow = string.Join("\t", model.Goal.Coefficients.Select(s => 0 - s));
-            return string.Format("LHS:\n{0}\n\nRHS:\n{1}\n\nObjective Row:\n{2}", lhs, rhs, objectiveRow);
+            //var rhs = string.Join("\n", model.Constraints.Select(s => s.Value));
+            //var lhs = string.Join("\n", model.Constraints.Select(s => string.Join("\t", s.Coefficients)));
+            //var objectiveRow = string.Join("\t", model.Goal.Coefficients.Select(s => 0 - s));
+            var lhs = Matrix<double>.Build.DenseOfRowArrays(model.Constraints.Select(s => s.Coefficients));
+            var rhs = Vector<double>.Build.DenseOfEnumerable(model.Constraints.Select(s => s.Value));
+            var objectiveRow = Vector<double>.Build.DenseOfEnumerable(model.Goal.Coefficients.Concat<double>(new double[] { model.Goal.ConstantTerm }));
+            return string.Format("LHS:\n{0}\n\nRHS:\n{1}\n\nObjective Row:\n{2}", lhs, rhs, objectiveRow.ToRowMatrix());
         }
     }
 }
